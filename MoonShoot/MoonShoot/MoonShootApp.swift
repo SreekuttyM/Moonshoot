72b3db5126d0b21fe136504167d1098e04c3d737
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-26 18:04:50 +0400

    project setup added

---
+++ MoonShoot/MoonShoot/MoonShootApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShootApp.swift
+//  MoonShoot
+//
+//  Created by Sreekutty Maya on 26/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonShootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
