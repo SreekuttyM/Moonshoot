8ee2c65e87adac9d98123b1fbc26e24bee2b7291
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-26 18:55:34 +0400

    mission list added with scrollview LazyVGrid

---
+++ MoonShoot/MoonShoot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShoot
+//
+//  Created by Sreekutty Maya on 26/04/2024.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
